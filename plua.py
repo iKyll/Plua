66d880728163eb0c2407eee08e59e94cbd49c516
iKyll <veroveloevan@gmail.com>
2023-04-06 21:54:00 +0200

    Introduced parentheses

--- plua.py
+++ plua.py
@@ -34,7 +34,8 @@ class Op:
 
 class TokenType(Enum):
     KEYWORD=auto()
-    PAREN=auto()
+    LPAREN=auto()
+    RPAREN=auto()
     STR=auto()
     INT=auto()
 
@@ -53,12 +54,27 @@ class Parens:
 Program = List[Union[Token, Op, Parens]]
 
 def simulate(program: Program):
+    in_parens = False
+    if isinstance(program, Parens): 
+        program = program.ops
+        in_parens = True
+
+    if len(program) == 0:
+        print("ERROR: simulating empty code")
+        exit(1)
+
     for op in range(len(program)):
         token = program.pop(0)
         if token.typ in OpType:
             assert len(OpType) == 4, "Exhaustive handling of ops in simulate()"
             if token.typ == OpType.PRINT:
                 value = token.value
+                if isinstance(value, Parens):
+                    value = simulate(value)
+                    if len(value) > 1:
+                        print(f"{token.loc[0]}:{token.loc[1]}:{token.loc[2]}: ERROR: too many arguments for print operator")
+                        exit(1)
+                    value = value[0].value
                 if isinstance(value, str):
                     print(value.encode('latin-1', 'backslashreplace').decode('unicode-escape'))
                 else:
@@ -70,26 +86,47 @@ def simulate(program: Program):
                 if arg1.typ != arg2.typ:
                     print(f"{token.loc[0]}:{token.loc[1]}:{token.loc[2]}: ERROR: `+` operator can only add two arguments of the same type but found `{arg1.typ}` and `{arg2.typ}`")
                     exit(1)
-                if arg1.typ == TokenType.KEYWORD or arg1.typ == TokenType.PAREN:
+                if arg1.typ == TokenType.KEYWORD:
                     print(f"{token.loc[0]}:{token.loc[1]}:{token.loc[2]}: ERROR: `+` operator can only add strings or numbers.")
                     exit(1)
                 new_value = arg1.value + arg2.value
                 program.append(Token(arg1.typ, arg2.loc, new_value))
-            elif token.typ == OpType.LPAREN:
-                assert False, "not implemented"
-            elif token.typ == OpType.RPAREN:
-                assert False, "not implemented"
+            
 
     if len(program) != 0:
+        if in_parens: return program
         token = program.pop()
         print(f"{token.loc[0]}:{token.loc[1]}:{token.loc[2]}: ERROR: unhandled argument in program: `{token.value}` with type: `{token.typ}`")
         exit(1)
+
+# Implement the brackets
+def find_last_separator(program: Program) -> int:
+    if len(program) == 0:
+        print("ERROR: find_last_separator had an empty list")
+        exit(1)
+    if program[0].typ != TokenType.LPAREN:
+        print("ERROR: first argument in find_last_separator need to be a separator")
+        exit(1)
+    needed = 1
+    ip = 1
+    for op in program[1:]:
+        if op.typ == TokenType.LPAREN: needed += 1
+        elif op.typ == TokenType.RPAREN:
+            needed -= 1
+            if needed == 0:
+                return ip
+        ip += 1
+    # Find a way to get token's loc
+    print("ERROR: parentheses not closed, missing: ", needed)
+    exit(1)
 
 # This function checks for all tokens if it's a KEYWORD, if it is a keyword it changes that token into an op
 def parse_token_as_op(tokens: List[Token]) -> Program:
     for ip, token in enumerate(tokens):
         if token.value in SEPARATORS:
-            assert False, "not implemented"
+            loc = token.loc
+            value = parse_token_as_op(tokens[ip+1:-1])
+            return Parens(loc, value)
         if token.value in KEYWORDS or token.value in KEYWORDS_SIGNS:
             typ = KEYWORDS_BY_NAME[token.value]
             assert len(OpType) == 4, "Exhaustive handling of ops in parse_token_as_op()"
@@ -98,11 +135,22 @@ def parse_token_as_op(tokens: List[Token]) -> Program:
                     print("%s:%d:%d: ERROR: expected argument but found EOF " % token.loc)
                     exit(1)
                 # TODO: find a way to change this pop because it's quite slow 
-                arg = tokens.pop(ip+1)
-                if (arg.typ != TokenType.STR) and (arg.typ != TokenType.INT):
-                    print(f"{arg.loc[0]}:{arg.loc[1]}:{arg.loc[2]}: ERROR: expected string or number but found `{arg.typ}`")
-                    exit(1)
-                tokens[ip] = Op(OpType.PRINT, token.loc, arg.value)
+                arg = tokens[ip+1]
+                if arg.typ == TokenType.LPAREN:
+                    closing_index = find_last_separator(tokens[ip+1:])
+                    arg = parse_token_as_op(tokens[ip+1:closing_index+2])
+                    for i in range(closing_index+1):
+                        _ = tokens.pop(0)
+                else:
+                    _ = tokens.pop(ip+1)
+
+                if isinstance(arg, Parens):
+                    tokens[ip] = Op(OpType.PRINT, token.loc, arg)
+                else:
+                    if (arg.typ != TokenType.STR) and (arg.typ != TokenType.INT):
+                        print(f"{arg.loc[0]}:{arg.loc[1]}:{arg.loc[2]}: ERROR: expected string or number but found `{arg.typ}`")
+                        exit(1)
+                    tokens[ip] = Op(OpType.PRINT, token.loc, arg.value)
             elif typ == OpType.PLUS:
                 if tokens[0] == token:
                     print(f"{token.loc[0]}:{token.loc[1]}:{token.loc[2]}: ERROR: expected one argument before the operator but found nothing.")
@@ -143,7 +191,13 @@ def find_index(line: str, start, space=True, quotes=False):
 def lex_line(line: str) -> Tuple[int, TokenType, Union[str, int]]:
     col = find_index(line, 0)
     while col < len(line):
-        if line[col] == '"':
+        if line[col] == '(':
+            yield (col, TokenType.LPAREN, '(')
+            col = find_index(line, col + 1)
+        elif line[col] == ')':
+            yield (col, TokenType.RPAREN, ')')
+            col = find_index(line, col + 1)
+        elif line[col] == '"':
             end_word = find_index(line, col+1, space=False, quotes=True) 
             if line[end_word] != '"': assert False, "Lexer error ?"
             yield (col, TokenType.STR, line[col+1:end_word])
